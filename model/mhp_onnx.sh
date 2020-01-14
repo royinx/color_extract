7db80b7ce35b70bf6a515264d09de20b6dcf1750
royinx <royfork@hotmail.com>
2020-01-14 15:55:20 +0800

    update sh

--- model/mhp_onnx.sh
+++ model/mhp_onnx.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 wget -O FCN-ResNet18-MHP-512x320.tar.gz https://nvidia.box.com/shared/static/dgaw0ave3bdws1t5ed333ftx5dbpt9zv.gz
+tar -zxvf FCN-ResNet18-MHP-512x320.tar.gz
+rm FCN-ResNet18-MHP-512x320.tar.gz
 wget -O FCN-ResNet18-MHP-640x360.tar.gz https://nvidia.box.com/shared/static/50mvlrjwbq9ugkmnnqp1sm99g2j21sfn.gz
+tar -zxvf FCN-ResNet18-MHP-640x360.tar.gz
+rm FCN-ResNet18-MHP-640x360.tar.gz
