3c1f319ed7d63d70d04393c12ef6a29ce4346e0f
royinx <royfork@hotmail.com>
2020-01-14 15:49:01 +0800

    model download

---
+++ model/mhp_onnx.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+wget -O FCN-ResNet18-MHP-512x320.tar.gz https://nvidia.box.com/shared/static/dgaw0ave3bdws1t5ed333ftx5dbpt9zv.gz
+wget -O FCN-ResNet18-MHP-640x360.tar.gz https://nvidia.box.com/shared/static/50mvlrjwbq9ugkmnnqp1sm99g2j21sfn.gz
